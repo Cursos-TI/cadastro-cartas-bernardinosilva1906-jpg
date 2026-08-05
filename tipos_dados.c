38b21133f4d98ef842eb99a49384b20239f7b18e
João Carlos Bernardino da Silva <bernardinosilva1906@gmail.com>
2026-08-05 17:37:45 -0300

    Adiciona exercício sobre tipos de dados

---
+++ tipos_dados.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int idade = 42;             
+float altura = 1.86f;       
+double salario = 2500.75;   
+char inicial = 'J';         
+    printf("Idade: %d\n", idade);
+    printf("Altura: %.2f\n", altura);
+    printf("Salário: %.2lf\n", salario);
+    printf("Inicial: %c\n", inicial);
+
+    return 0;
+}   
+
